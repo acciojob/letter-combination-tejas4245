e1d813413e5c88d2fe6e877e6dc7af0a27bd3752
tejas4245 <120262626+tejas4245@users.noreply.github.com>
2023-03-22 12:02:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	if(!input_digit){
+		return [];
+	}
+	const phoneMap={
+		"2": "abc",
+	    "3": "def",
+		"4": "ghi",
+		"5": "jkl",
+		"6": "mno",
+		"7": "pqrs",
+		"8": "tuv",
+		"9": "wxyz"
+	};
+	let result=[""];
+	for(let i=0;i<input_igit.length;i++){
+		const letters=phoneMap[input_digit[i]];
+		const newResult=[];
+		for(let j=0;j<letters.length;i++){
+			for(let k=0;k<result[k]+letters[j]);
+		}
+	}
+	result=newResult;
+}
+return result.sort();
 }
 
 module.exports = letterCombinations;
